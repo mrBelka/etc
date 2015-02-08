55729839ac1788530308d5e9f0881aa7663a1ba9
mrBelka <mishklgpmi@mail.ru>
2015-02-08 17:28:16 +0300

    lab9

--- lab9.dpr
+++ lab9.dpr
@@ -9,11 +9,16 @@ var
 
 begin
     Readln(n);
+    //считываем все числа
     for i:=1 to n do begin
         Read(x);
+        //делаем копию считанного числа
         tmp:=x;
+        //запоминаем последнюю цифру
         last:=tmp mod 10;
         while tmp>0 do begin
+            //каждый раз запоминаем последнюю цифру числа
+            //на последнем шаге она будет первой цифрой
             first:=tmp mod 10;
             tmp:=tmp div 10;
         end;
